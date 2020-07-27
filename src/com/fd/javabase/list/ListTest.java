62b7a3a7145ba71376f781e04f1eae384b1897e1
zhangtao <samlinzhang@qq.com>
2020-07-27 20:16:05 +0800

    如果使用asList快速得到一个List,那么这个List是只读的,不能添加和删除元素

--- src/com/fd/javabase/list/ListTest.java
+++ src/com/fd/javabase/list/ListTest.java
@@ -3,6 +3,7 @@ package com.fd.javabase.list;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -33,5 +34,25 @@ public class ListTest {
          end=System.currentTimeMillis();
 
         System.out.println("LinkedList新增的时间是"+(end-start));
+    }
+    @Test
+    public void testArrayToList() throws Exception{
+        //如果使用asList快速得到一个List,那么这个List是只读的,不能添加和删除元素
+        List<String> strings = Arrays.asList("1", "2", "3");
+        strings.add("4");
+
+        List list = new ArrayList();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        list.add("4");
+
+        List list1 = list.subList(0, 2);
+        list1.add("5");
+        list1.remove("2");
+
+        for (Object o : list1) {
+            System.out.println("o = " + o);
+        }
     }
 }
