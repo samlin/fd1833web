989c4a5f01a0ff329d60bc94200c6c63abd0b38f
zhangtao <samlinzhang@qq.com>
2020-07-27 20:22:00 +0800

    如果在调用泛型的时候,不传入任何参数,那么默认的泛型类型是Object

---
+++ src/com/fd/javabase/list/AnimalTest.java
@@ -0,0 +1,11 @@
+package com.fd.javabase.list;
+
+public class AnimalTest {
+    public static void main(String[] args) {
+        //如果在调用泛型的时候,不传入任何参数,那么默认的泛型类型是Object
+        Animal animal=new Animal();
+        animal.setT("5");
+
+
+    }
+}
