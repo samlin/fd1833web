ce398cfdf7fa1151b9f7637314c55126991e6127
zhangtao <samlinzhang@qq.com>
2020-08-04 08:47:44 +0800

    定义Servlet的声明周期的init方法,但是要注意重写有参的init(ServletConfig config)

--- src/com/fd/servlet/FdServlet.java
+++ src/com/fd/servlet/FdServlet.java
@@ -12,6 +12,17 @@ public class FdServlet extends javax.servlet.http.HttpServlet {
         System.out.println("FdServlet.FdServlet");
     }
 
+    /**
+     * 定义Servlet的声明周期的init方法,但是要注意重写有参的init(ServletConfig config)
+     * @param config
+     * @throws ServletException
+     */
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        System.out.println("FdServlet.init");
+    }
+
     @Override
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
         System.out.println("FdServlet.doPost");
