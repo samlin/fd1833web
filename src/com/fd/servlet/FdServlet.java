c23105b7459730da693129e78fabd51d63322b2f
zhangtao <samlinzhang@qq.com>
2020-08-04 08:45:32 +0800

    定义构造器

--- src/com/fd/servlet/FdServlet.java
+++ src/com/fd/servlet/FdServlet.java
@@ -5,6 +5,12 @@ import javax.servlet.ServletException;
 import java.io.IOException;
 
 public class FdServlet extends javax.servlet.http.HttpServlet {
+    /**
+     * 定义Servlet的构造器
+     */
+    public FdServlet() {
+        System.out.println("FdServlet.FdServlet");
+    }
 
     @Override
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
