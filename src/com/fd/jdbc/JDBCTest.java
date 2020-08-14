cef65c99b174863dc32248966777525e6162282d
zhangtao <samlinzhang@qq.com>
2020-08-14 11:35:31 +0800

    通过内联把只用了一次的变量直接传入

--- src/com/fd/jdbc/JDBCTest.java
+++ src/com/fd/jdbc/JDBCTest.java
@@ -14,8 +14,8 @@ public class JDBCTest extends SuperJDBCTest {
 
     @Test
     public void testConnection() throws Exception {
-        String sql = "update users set name='zhangsan789' where id=1";
-        executeSql(sql);
+        //通过内联把只用了一次的变量直接传入
+        executeSql("update users set name='zhangsan789' where id=1");
     }
 
 }
