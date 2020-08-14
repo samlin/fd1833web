3ca1a8ef2802d15bae05d91419357eed14e1f29d
zhangtao <samlinzhang@qq.com>
2020-08-14 11:14:48 +0800

    把局部变量抽取成字段变量,方便别的方法也共享conneciton

--- src/com/fd/jdbc/JDBCTest.java
+++ src/com/fd/jdbc/JDBCTest.java
@@ -6,14 +6,19 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
 
+@SuppressWarnings("ALL")
 public class JDBCTest {
+
+    //把局部变量抽取成字段变量,方便别的方法也共享conneciton
+    private Connection connection;
+
     @Test
     public void testConnection() throws Exception{
 //        1.注册驱动
         Class.forName("com.mysql.jdbc.Driver");
 
 //        2.得到连接对象
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/db1833", "root", "");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/db1833", "root", "");
         System.out.println("connection = " + connection);
 
 //       3.得到执行Sql的statement
