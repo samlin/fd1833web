93d7c6e16d3244936c765a265dbe1de605d9b11e
zhangtao <samlinzhang@qq.com>
2020-08-14 11:21:39 +0800

    因为每次执行方法时,Statement都不一样,所以每次在做单元测试时候,Statement都要重新构建一个(加上@Before即可)

--- src/com/fd/jdbc/JDBCTest.java
+++ src/com/fd/jdbc/JDBCTest.java
@@ -1,5 +1,6 @@
 package com.fd.jdbc;
 
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -14,14 +15,14 @@ public class JDBCTest {
     //把局部变量抽取成字段变量,方便别的方法也共享conneciton
     private static Connection connection;
 
-    @Test
-    public void testConnection() throws Exception{
+    //    把statement抽取成一个字段对象,
+    private Statement statement;
 
-//       3.得到执行Sql的statement
-        Statement statement = connection.createStatement();
+    @Test
+    public void testConnection() throws Exception {
 
 //      4.执行Sql语句
-        int i = statement.executeUpdate("update users set name='zhangsan' where id=1");
+        int i = statement.executeUpdate("update users set name='zhangsan123' where id=1");
 
 //      5.关闭statement connection
         statement.close();
@@ -29,7 +30,18 @@ public class JDBCTest {
     }
 
     /**
+     * 因为每次执行方法时,Statement都不一样,所以每次在做单元测试时候,Statement都要重新构建一个(加上@Before即可)
+     * @throws SQLException
+     */
+    @Before
+    public void initStatement() throws SQLException {
+        //       3.得到执行Sql的statement
+        statement = connection.createStatement();
+    }
+
+    /**
      * 把Connection初始化抽取到一个函数中,在类初始化的时候只加载一次
+     *
      * @throws ClassNotFoundException
      * @throws SQLException
      */
