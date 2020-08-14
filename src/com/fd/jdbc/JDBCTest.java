2f2374441acb10b11687e2e6551f93de91ab9944
zhangtao <samlinzhang@qq.com>
2020-08-14 10:52:32 +0800

    @BeforeClass 在当前所有的单元测试执行之前只执行一次

--- src/com/fd/jdbc/JDBCTest.java
+++ src/com/fd/jdbc/JDBCTest.java
@@ -2,9 +2,22 @@ package com.fd.jdbc;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class JDBCTest {
+    /**
+     * @BeforeClass 在当前所有的单元测试执行之前只执行一次
+     * @throws Exception
+     */
+    @BeforeClass
+    public static void beforeClass() throws Exception{
+        System.out.println("JDBCTest.beforeClass>>>>>>>>>>>>>>>>");
+    }
+
+    /**
+     * @Before是每次单元测试之前都要执行一次
+     */
     @Before
     public void before() throws Exception{
         System.out.println("JDBCTest.before---------");
