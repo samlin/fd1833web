dc1839ae342f8101051c8fa5310c2e24127c2911
zhangtao <samlinzhang@qq.com>
2020-08-14 11:05:35 +0800

    3.得到执行Sql的statement

--- src/com/fd/jdbc/JDBCTest.java
+++ src/com/fd/jdbc/JDBCTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Statement;
 
 public class JDBCTest {
     @Test
@@ -14,5 +15,10 @@ public class JDBCTest {
 //        2.得到连接对象
         Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/db1833", "root", "");
         System.out.println("connection = " + connection);
+
+//       3.得到执行Sql的statement
+        Statement statement = connection.createStatement();
+
+
     }
 }
