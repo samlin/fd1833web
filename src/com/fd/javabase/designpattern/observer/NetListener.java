6e6eaa1910c12feec7dd1b282d7e3ccef2ed4dca
zhangtao <samlinzhang@qq.com>
2020-08-18 17:12:47 +0800

    重构原来代码,把普通项目改成maven项目,把源文件从src开始改到从src/main/java开始

--- src/com/fd/javabase/designpattern/observer/NetListener.java
+++
@@ -1,8 +0,0 @@
-package com.fd.javabase.designpattern.observer;
-
-public class NetListener implements Listener {
-    @Override
-    public void update(String message) {
-        System.out.println("发送网络通知:--> "+message);
-    }
-}
