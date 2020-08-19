b69116805ed814b9e14687c97f900a1ad79692ef
zhangtao <samlinzhang@qq.com>
2020-08-19 10:53:05 +0800

    在DAO定义了getUserName的伪实现

--- src/main/java/com/fd/dao/impl/UserDAOImpl.java
+++ src/main/java/com/fd/dao/impl/UserDAOImpl.java
@@ -2,12 +2,14 @@ package com.fd.dao.impl;
 
 import com.fd.dao.UserDAO;
 
+import java.util.Date;
+
 /**
  * 定义MVC中Model层的实现类
  */
 public class UserDAOImpl implements UserDAO {
     @Override
     public String getUserName() {
-        return null;
+        return "FD User Name "+new Date();
     }
 }
