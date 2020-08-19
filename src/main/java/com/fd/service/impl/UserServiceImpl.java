d176419fa31990b8ae451723abc97dd88c37d9af
zhangtao <samlinzhang@qq.com>
2020-08-19 10:50:36 +0800

    在Service层调用DAO层

--- src/main/java/com/fd/service/impl/UserServiceImpl.java
+++ src/main/java/com/fd/service/impl/UserServiceImpl.java
@@ -1,13 +1,18 @@
 package com.fd.service.impl;
 
+import com.fd.dao.UserDAO;
+import com.fd.dao.impl.UserDAOImpl;
 import com.fd.service.UserService;
 
 /**
  * 定义MVC的Service层的实现类
  */
 public class UserServiceImpl implements UserService {
+
+    //在Service中定义DAO
+    private UserDAO userDAO = new UserDAOImpl();
     @Override
     public String getUserName() {
-        return null;
+        return "Service--> "+userDAO.getUserName();
     }
 }
