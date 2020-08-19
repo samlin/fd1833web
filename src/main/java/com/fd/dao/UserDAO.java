4567ffa8b9307581f05564bd789e6c2d79d18a23
zhangtao <samlinzhang@qq.com>
2020-08-19 10:50:56 +0800

    定义MVC模式中DAO层

---
+++ src/main/java/com/fd/dao/UserDAO.java
@@ -0,0 +1,8 @@
+package com.fd.dao;
+
+/**
+ * 定义MVC中的Model层,UserDAO
+ */
+public interface UserDAO {
+    String getUserName();
+}
---
+++ src/main/java/com/fd/dao/impl/UserDAOImpl.java
@@ -0,0 +1,13 @@
+package com.fd.dao.impl;
+
+import com.fd.dao.UserDAO;
+
+/**
+ * 定义MVC中Model层的实现类
+ */
+public class UserDAOImpl implements UserDAO {
+    @Override
+    public String getUserName() {
+        return null;
+    }
+}
