2cf1a2565cf2695b7c5ba57b2c897440c6029943
zhangtao <samlinzhang@qq.com>
2020-08-21 08:44:56 +0800

    继续测试

--- src/main/java/com/fd/Main.java
+++ src/main/java/com/fd/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("写测试代码");
-        System.out.println(" =              " );
+        System.out.println(" =                " );
     }
 }
